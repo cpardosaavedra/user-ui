d53b81c41a78924604e901d852727c6b6f5037a9
Prina <Arisnel.Prina@sovos.com>
2023-06-27 12:18:38 -0400

    se agrega css y js a home queda pendiente revision de conexion y continuar con el js

---
+++ js/home.js
@@ -0,0 +1,34 @@
+function searchUser(){
+
+fetch('http://localhost:8080/getUserData', {
+  method: 'GET'
+})
+  .then(response => response.json())
+  .then(data => {
+        // Obtener referencia a la tabla
+   console.log(data)
+    const table = document.getElementById('data-table');
+
+    // Limpiar el contenido existente en el cuerpo de la tabla
+    table.getElementsByTagName('tbody')[0].innerHTML = '';
+
+    // Iterar sobre los datos recibidos
+    data.forEach(user => {
+      // Crear una nueva fila en la tabla
+      const newRow = table.insertRow();
+
+      // Insertar celdas en la fila con los datos correspondientes
+      newRow.insertCell().textContent = user.nombre;
+      newRow.insertCell().textContent = user.apellido;
+      newRow.insertCell().textContent = user.email;
+      newRow.insertCell().textContent = user.rut;
+      newRow.insertCell().textContent = user.direccion;
+      newRow.insertCell().textContent = user.nickname;
+    });
+  })
+  .catch(error => {
+    // Manejar errores
+    console.error('Error:', error);
+    console.log("¡No estás conectado al endpoint!");
+  });
+}
