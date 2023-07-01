e043913a6bbb4d87329934c491048fddca6fc1fa
Claudio Pardo <claudiopardo.globant@latam.com>
2023-07-01 01:19:56 -0400

    se implementa el camino de error, falta llenar los datos del formulario

--- js/home.js
+++ js/home.js
@@ -1,45 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   var urlParams = new URLSearchParams(window.location.search);
 
   let token = urlParams.get("token");
 
-fetch('http://localhost:8080/getUserData', {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json',
-    'session-token': token
-  }
-})
-  .then(response => response.json())
-  .then(data => {
-        // Obtener referencia a la tabla
-
-    document.getElementById("nombre").value = data.name;
-   console.log(data)
-    const table = document.getElementById('data-table');
-
-    // Limpiar el contenido existente en el cuerpo de la tabla
-    table.getElementsByTagName('tbody')[0].innerHTML = '';
-
-    // Iterar sobre los datos recibidos
-    data.forEach(user => {
-      // Crear una nueva fila en la tabla
-      const newRow = table.insertRow();
-    
-
-      // Insertar celdas en la fila con los datos correspondientes
-      newRow.insertCell().textContent = user.nombre;
-      newRow.insertCell().textContent = user.apellido;
-      newRow.insertCell().textContent = user.email;
-      newRow.insertCell().textContent = user.rut;
-      newRow.insertCell().textContent = user.direccion;
-      newRow.insertCell().textContent = user.nickname;
-    });
+  fetch('http://localhost:8080/getUserData', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'session-token': token
+    }
   })
-  .catch(error => {
-    // Manejar errores
-    console.error('Error:', error);
-    console.log("¡No estás conectado al endpoint!");
-  });
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      } else {
+        return response.json();
+      }
+    })
+    .then(responseBody => {
+        console.log(responseBody);
+      
+        //manipular datos del response body y llenar el formulario del html
+        //recuerda que no es necesario ocupar for para esto
+    })
+    .catch(error => {
+      // Manejar errores
+      console.log(error);
+
+      if (error.message == 400) {
+        document.getElementById("error-message").innerHTML = "¡Sesión expirada! Por favor, vuelve a iniciar sesión.";
+      } else {
+        document.getElementById("error-message").innerHTML = "Error en la applicación, vuelve a iniciar sesión.";
+      }
+
+      showSessionExpiredPopUp();
+    });
 });
+
+function showSessionExpiredPopUp() {
+  document.getElementById("session-expired-overlay").style.display = "flex";
+  document.getElementById("session-expired-message").style.display = "flex";
+}
