cff3d2aa3bf652a2bde5434723f7d118c710f84e
Prina <Arisnel.Prina@sovos.com>
2023-07-03 10:42:56 -0400

    se agrega carpeta pruebaPokeApi 'temporalmente', solo uso de pruebas

---
+++ pruebaPokeApi/index.js
@@ -0,0 +1,28 @@
+fetch ('https://pokeapi.co/api/v2/pokemon/charmander',{
+    method: 'GET',
+    headers: {
+        'content-Type':'application/json',
+    },
+    body: JSON.stringify()
+})
+
+.then(response => {
+    if(!response.ok) {
+        throw new Error('No se puedo obtener la información del pokémon');        
+       }   return response.json()
+    
+})
+.then(data =>{
+    console.log('Información del pokémon: ',data);
+    console.log('Nombre del Pokémon: ', data.name);
+    console.log('Altura del pokemon:', data.height);
+    console.log('Peso del Pokémon: ',data.weight);
+
+})
+.catch(error =>{
+    console.error('Error:', error.message);
+})
+
+// de apoco voy aprendiendo a usar fetch :D 
+
+//unos ejemplos mas y aprendere bien :D 
