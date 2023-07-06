278179a9aeb4eca2ce36f60f9f9580ce92d493cd
Prina <Arisnel.Prina@sovos.com>
2023-07-06 00:08:59 -0400

    se muestra informacion por pantalla al presionar search y escoger al pokemonm

--- test/pokeInfo/search.js
+++ test/pokeInfo/search.js
@@ -1,5 +1,6 @@
 function getData() {
     var pokeName = document.getElementById("pokemonName").value;
+    
 
     console.log("el pokemon seleccionado: " + pokeName);
 
@@ -22,12 +23,21 @@ function getData() {
     console.log('Nombre del Pokémon: ', data.name);
     console.log('Altura del pokemon:', data.height);
     console.log('Peso del Pokémon: ',data.weight);
-    document.getElementById("showPokeData").innerHTML = JSON.stringify(data);
+    // document.getElementById("showPokeData").innerHTML = JSON.stringify(data); 
     //mostrar info en la pagina web ya sea en una table, o en la wea te salga de los cojones, pero muestrala y
     //se tiene que actualizar, cada vez que yo busque un pokemon diferente
+        document.getElementById("namepoke").innerHTML = data.name;
+        document.getElementById("typePoke").innerHTML = data.types[0].type.name;;
+        document.getElementById("height").innerHTML = data.height;
+        document.getElementById("weight").innerHTML = data.weight;
+
+    
     //si no se encuntra, mostrar un mensaje en pantalla diciendo: pokemon "pokemon" no encontrado.
 })
 .catch(error =>{
     console.error('Error:', error.message);
+    console.log(pokeName+" Este Pokémon no ha sido encontrado");
 })
 }
+
+
