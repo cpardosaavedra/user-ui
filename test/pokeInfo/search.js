eca0f6de24818be8e3eb003fbd0fa562898dc272
cpardosaavedra <52170227+cpardosaavedra@users.noreply.github.com>
2023-07-17 22:37:54 -0400

    Feature/crear usuario (#7)
    * se agrega carpeta pruebaPokeApi 'temporalmente', solo uso de pruebas
    
    * search pokemon test
    
    * se realiza pequeño cambio en pruebaPokeApi
    
    * se muestra informacion por pantalla al presionar search y escoger al pokemonm
    
    * se crea if para verificar si existe el pokemon ingresado
    
    * Se agrega archivo combobox.js
    
    ---------
    
    Co-authored-by: Prina <Arisnel.Prina@sovos.com>
    Co-authored-by: Claudio Pardo <claudiopardo.globant@latam.com>

---
+++ test/pokeInfo/search.js
@@ -0,0 +1,46 @@
+function getData() {
+    var pokeName = document.getElementById("pokemonName").value;
+    
+
+    console.log("el pokemon seleccionado: " + pokeName);
+
+    fetch ('https://pokeapi.co/api/v2/pokemon/' + pokeName,{
+    method: 'GET',
+    headers: {
+        'content-Type':'application/json',
+    },
+    body: JSON.stringify()
+})
+
+.then(response => {
+    if(!response.ok) {
+        throw new Error('No se puedo obtener la información del pokémon');        
+       }   return response.json()
+    
+})
+.then(data =>{
+    console.log('Información del pokémon: ',data);
+    console.log('Nombre del Pokémon: ', data.name);
+    console.log('tipo del Pokémon: ', data.types[0].type.name);
+    console.log('Altura del pokemon:', data.height);
+    console.log('Peso del Pokémon: ',data.weight);
+    // document.getElementById("showPokeData").innerHTML = JSON.stringify(data); 
+    //mostrar info en la pagina web ya sea en una table, o en la wea te salga de los cojones, pero muestrala y
+    //se tiene que actualizar, cada vez que yo busque un pokemon diferente
+if(pokeName.trim() === data.name.trim()){    
+    document.getElementById("namepoke").innerHTML = data.name;
+    document.getElementById("typePoke").innerHTML = data.types[0].type.name;
+    document.getElementById("height").innerHTML = data.height;
+    document.getElementById("weight").innerHTML = data.weight;     
+    //si no se encuentra, mostrar un mensaje en pantalla diciendo: pokemon "pokemon" no encontrado.
+}else{
+    alert(pokeName + ' Pokémon no encontrado') ;    
+}
+})
+.catch(error =>{
+    console.error('Error:', error.message);
+    console.log(pokeName+" Este Pokémon no ha sido encontrado");
+})
+}
+
+
