2cf290e503c75106c2cc700e5aebd43fc5a7a7fe
cpardosaavedra <52170227+cpardosaavedra@users.noreply.github.com>
2023-08-30 22:02:01 -0400

    Feature/combo box filtered (#9)
    * se agrega carpeta pruebaPokeApi 'temporalmente', solo uso de pruebas
    
    * search pokemon test
    
    * se realiza pequeño cambio en pruebaPokeApi
    
    * se muestra informacion por pantalla al presionar search y escoger al pokemonm
    
    * se crea if para verificar si existe el pokemon ingresado
    
    * Se agrega archivo combobox.js
    
    * se agrega imagen del pokemon al buscar en combobox
    
    * se agrega servicio de evolucion pokemon (falta imagenes)
    
    * ejemplo de combo box filtrados
    
    * se agrega tercer combobox con filtro a modificar
    
    * se modigica filtro de evoluciones solo falta que muestre hacia abajo el combo box
    
    * se intenta filtrar las evoluciones sin exito (se seguira intentando)
    
    * se modifica tercer combobox (falta ver el filtro por que no cambia a los otros pokes)
    
    * se identifica falla en filtros para evolucion
    
    * se agregan pokemones faltantes a los filtros
    
    * se agregan ataques de pokemon
    
    * se agregan pequeñas modificaciones y se intenta agregar informacion a un parrafo
    
    * revisar por que me entrega undefined
    
    * se repara problema con combo box de ataque
    
    ---------
    
    Co-authored-by: Prina <Arisnel.Prina@sovos.com>
    Co-authored-by: Claudio Pardo <claudiopardo.globant@latam.com>
    Co-authored-by: arisnel21 <arisnel21@gmail.com>

---
+++ test/combo/combo.js
@@ -0,0 +1,450 @@
+// Evento que se ejecuta cuando se carga el contenido del DOM
+document.addEventListener("DOMContentLoaded", llenarCombo);
+
+let tipo = [
+    {
+        "id": 1, 
+        "value": "electrico"
+    }, 
+    {
+        "id": 2, 
+        "value": "tierra"
+    }, 
+    {
+        "id": 3, 
+        "value": "fuego"
+    }, 
+    {
+        "id": 4, 
+        "value": "agua"
+    }
+];
+
+let pokemon = [
+    {
+        "tipo": 1,
+        "nombre": "pikachu",
+        "id": 1       
+    },
+    {
+        "tipo": 1,
+        "nombre": "voltorb",
+        "id": 2         
+    },
+    {
+        "tipo": 1,
+        "nombre": "magnamite",         
+        "id": 3
+    },
+    {
+        "tipo": 1,
+        "nombre": "zapdos",        
+        "id": 4
+    },
+
+    {
+        "tipo": 2,
+        "nombre": "sandshrew",  
+        "id": 5     
+    },
+    {
+        "tipo": 2,
+        "nombre": "rhyhorn",
+        "id": 6
+    },
+    {
+        "tipo": 2,
+        "nombre": "diglet",
+        "id": 7
+    },
+    {
+        "tipo": 2,
+        "nombre": "cubone" ,  
+        "id": 8      
+    },
+
+    {
+        "tipo": 3,
+        "nombre": "vulpix",   
+        "id": 9     
+    },
+    {
+        "tipo": 3,
+        "nombre": "growlithe",   
+        "id": 10      
+    },
+    {
+        "tipo": 3,
+        "nombre": "charmander" ,  
+        "id": 11     
+    },
+    {
+        "tipo": 3,
+        "nombre": "torchic",    
+        "id": 12     
+    },
+    
+    {
+        "tipo": 4,
+        "nombre": "squirtle",  
+        "id": 13       
+    },
+    {
+        "tipo": 4,
+        "nombre": "totodile",  
+        "id": 14       
+    },
+    {
+        "tipo": 4,
+        "nombre": "magikarp",  
+        "id": 15     
+    },
+    {
+        "tipo": 4,
+        "nombre": "psyduck",   
+        "id": 16    
+    },
+]
+
+let evolution = [
+    {
+        "id": 1,              
+        "evoluciones": [
+            {
+                "nombre": "Pichu"
+            },
+            {
+                "nombre": "Raichu"
+            },         
+        ]
+    },{
+        "id": 2,        
+        "evoluciones": [
+        {
+            "nombre": "electrode",            
+        },               
+    ]
+},
+{
+    "id": 3,    
+    "evoluciones": [
+        {
+            "nombre": "magnetone"
+        },
+        {
+            "nombre": "magnezone"
+        },             
+    ]
+},
+{
+    "id": 4,        
+    "evoluciones": [
+        {
+            "nombre": "zapdos",           
+        },
+    ]
+},{
+    "id": 5,
+    "evoluciones" : [
+        {
+            "nombre": "Sandslash"
+        }
+    ]
+},
+{
+    "id": 6,
+    "evoluciones" : [
+        {
+            "nombre": "Rydhon"
+        }
+    ]
+}
+,
+{
+    "id": 7,
+    "evoluciones" : [
+        {
+            "nombre": "Dugtrio"
+        }
+    ]
+},
+{
+    "id": 8,
+    "evoluciones" : [
+        {
+            "nombre": "Marowak"
+        },
+        {
+            "nombre": "Marowak-alola"
+        }
+    ]
+},
+{
+    "id": 9,
+    "evoluciones" : [
+        {
+            "nombre": "Ninetales"
+        },
+        {
+            "nombre": "Ninetales-Alola"
+        }
+    ]
+},
+{
+    "id": 10,
+    "evoluciones" : [
+        {
+            "nombre": "Arcanine"
+        }
+    ]
+},
+{
+    "id": 11,
+    "evoluciones" : [
+        {
+            "nombre": "Charmeleon"
+        },
+        {
+            "nombre": "Charizard"
+        }
+    ]
+},
+{
+    "id": 12,
+    "evoluciones" : [
+        {
+            "nombre": "Combusken"
+        },
+        {
+            "nombre": "Blaziken"
+        }
+    ]
+},
+{
+    "id": 13,
+    "evoluciones" : [
+        {
+            "nombre": "Wartortle"
+        },
+        {
+            "nombre": "Blastoise"
+        }
+    ]
+},
+{
+    "id": 14,
+    "evoluciones" : [
+        {
+            "nombre": "Croconaw"
+        },
+        {
+            "nombre": "Feraligart"
+        }
+    ]
+},
+{
+    "id": 15,
+    "evoluciones" : [
+        {
+            "nombre": "Gyarados"
+        }
+    ]
+},
+{
+    "id": 16,
+    "evoluciones" : [
+        {
+            "nombre": "Golduck"
+        }
+    ]
+}
+];
+
+let atkPokemon = [
+    {
+        "id": 1,
+        "ataque": [
+            {
+                "id":1,
+                "nombre": "Impactrueno"
+            },
+            {
+                "id":2,
+                "nombre": "Atactrueno"
+            },
+            {
+                "id":3,
+                "nombre": "Rayo"
+            }
+        
+        ]
+    },
+    {
+        "id": 2,
+        "ataque" : [
+            {
+                "nombre" : "golpe roca",
+            },
+            {
+                "nombre" : "roca afilada",
+            },
+            {
+                "nombre" : "tormenta de arena"
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "ataque" : [
+            {
+                "nombre" : "Lanza llamas",
+            },
+            {
+                "nombre" : "Giro fuego",
+            },
+            {
+                "nombre" : "tormenta de arena"
+            }
+        ]
+    },
+    {
+        "id": 4,
+        "ataque" : [
+            {
+                "nombre" : "Chorro de agua",
+            },
+            {
+                "nombre" : "pistola de agua",
+            },
+            {
+                "nombre" : "Hidrobomba"
+            }
+        ]
+    }
+]
+
+
+
+// Función para llenar el combo de tipos de Pokémon al cargar la página
+function llenarCombo() {
+    var tipoCombo = document.getElementById("tipo");
+
+    tipo.forEach(function(item) {
+        var option = document.createElement("option");
+        option.text = item.value;
+        option.id = item.id;
+        tipoCombo.appendChild(option);
+    });
+
+    getTipo(); // Llenar el combo de Pokémon una vez que se han cargado los tipos
+}
+
+// Función para filtrar y llenar el combo de Pokémon según el tipo seleccionado
+function getTipo() {
+    let tipoCombo = document.getElementById("tipo");
+    let tipoSelected = tipoCombo.options[tipoCombo.selectedIndex];
+
+    let pokemonsFilter = [];
+
+    for (var i = 0; i < pokemon.length; i++) {
+        let pokemonSelected = pokemon[i];
+
+        if (tipoSelected.id == pokemonSelected.tipo) {
+            pokemonsFilter.push(pokemonSelected);
+        }
+    }
+    
+    let pokemonCombo = document.getElementById("addpokemon")
+    pokemonCombo.innerHTML = "";
+
+    pokemonsFilter.forEach(function(item) {
+        var option = document.createElement("option");
+        option.text = item.nombre;
+        option.id = item.id;
+        pokemonCombo.appendChild(option);
+    });  
+    
+    console.log(pokemonCombo)
+    getEvolution ()
+    getAtaque()           
+}
+
+function getEvolution (){
+     // Filtro de evoluciones 
+     let getPokemon = document.getElementById("addpokemon");
+     let pokemonSet = getPokemon.options[getPokemon.selectedIndex];
+
+     let filterEvolution = [];
+
+     for(var i = 0; i < evolution.length; i++){
+         let evoList = evolution[i];
+
+         if (pokemonSet.id == evoList.id){                    
+             filterEvolution.push(evoList)
+             
+         }
+         
+     }    
+let evoCombo = document.getElementById("evo")
+evoCombo.innerHTML = "";
+
+filterEvolution.forEach(function(item) {
+    item.evoluciones.forEach(function(evo){
+        var option = document.createElement("option");            
+        option.text = evo.nombre; 
+        option.id = evo.id;          
+           
+        evoCombo.appendChild(option);
+        })   
+        console.log(evoCombo)     
+     });     
+}
+
+//realiza la comparacion de tipo.id y el atk.id
+function getAtaque(){
+    let typePoke  = document.getElementById("tipo");
+    let setPoke = typePoke.options[typePoke.selectedIndex];
+
+    let filterAtk = [];
+
+    for (var i = 0; i < atkPokemon.length; i++){
+        let atkList = atkPokemon[i];
+
+        if(setPoke.id == atkList.id ){
+            filterAtk.push(atkList)
+        }
+    }
+    // agrega los ataques encontrados segun el filtro realizado
+    let atkCombo = document.getElementById("atk");
+    atkCombo.innerHTML = "";
+
+    filterAtk.forEach(function(item){
+        item.ataque.forEach(function(addAtk){
+            var atkOption = document.createElement("option");
+            atkOption.text = addAtk.nombre;
+            atkOption.id = addAtk.id; 
+            atkCombo.appendChild(atkOption);
+            console.log(atkCombo)
+        })
+       
+    });    
+
+}   
+
+function guardar(){
+    getTipo();
+    console.log(setPoke)
+// let infoElement = document.getElementById("info");
+//     infoElement.textContent = `Tipo: ${setPoke.textContent}, Nombre del Pokémon: ${filterAtk.nombre}`;
+    let infoElement = document.getElementById("info");
+    infoElement.textContent = `Tipo: ${setPoke.textcontent}, Nombre del pokemon ${pokemonsFilter.nombre}` ;
+
+    // Aquí puedes hacer algo con el texto guardado, como mostrarlo en una alerta
+    alert("Información guardada:\n" + infoElement);
+}
+
+// intento agregar los datos de los pokemon en un parrafo.
+// mi meta para este paso es poder agregarlos a una tabla
+// de momento tengo que aprender como llamar una variable de una funcion
+// desde otra funcion 
+
